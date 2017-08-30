f6889a0266225709374a9bcc52a89eb177ac5f0f
fcc1885 <18829291885@163.com>
2017-08-30 22:50:49 +0800

    文件目录创建

---
+++ tasks/util/args.js
@@ -0,0 +1,33 @@
+import yargs from 'yargs';
+//区分是开发环境还是线上环境
+const args = yargs
+
+    .option('production', {
+        boolean: true,
+        default: false,
+        describe: 'min all script'
+    })
+
+    .option('watch', {
+        boolean: true,
+        default: false,
+        describe: 'watch all files'
+    })
+
+    .option('verbose',{
+        boolean:true,
+        default:false,
+        describe:'log',
+    })
+
+    .option('sourcemaps',{
+        describe:'force the creation of sroucemaps'
+    })
+
+    .option('port',{
+        string:true,
+        default:8080,
+        describe:'server port'
+    })
+
+    .argv
